57d2b5c84586503ff711d0a77c6a77da4aacd092
call-me-rodney <kwmrodney@gmail.com>
2025-12-17 11:05:51 +0300

    Environment variable mgt

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
@@ -8,9 +9,29 @@ import { UsersModule } from './users/users.module';
 import { ContactsModule } from './contacts/contacts.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { Email } from './email/email.model';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [EmailModule, AuthModule, UsersModule, ContactsModule, SequelizeModule.forRoot({dialect: 'postgres',host: 'localhost',port: 5432,username: 'postgres',password: 'postgres',database: 'myemail-service',models: [Email],}), AnalyticsModule],
+  imports: [
+    EmailModule, 
+    AuthModule, 
+    UsersModule, 
+    ContactsModule,
+    AnalyticsModule,
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      load: [configuration],
+      cache: true,
+    }), 
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: configuration.database.host,
+      port: configuration.database.port,
+      username: configuration.database.username,
+      password: configuration.database.password,
+      database: configuration.database.database,
+      models: [Email],}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/config/configuration.ts
@@ -0,0 +1,18 @@
+const config = {
+    development:{
+        port: parseInt(process.env.PORT, 10) || 3000,
+        database: {
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+        }
+    },
+    production:{
+        port: parseInt(process.env.PORT, 10) || 3000,
+        databaseUrl: process.env.DATABASE_URL,
+    }
+}
+
+export default config[process.env.NODE_ENV || 'development'];
