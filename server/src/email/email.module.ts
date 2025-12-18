14fefc6cc5252276e6e3e7edda2a2f6a5d514fc6
call-me-rodney <kwmrodney@gmail.com>
2025-12-19 00:40:21 +0300

    Adding new models to module imports

--- server/src/email/email.module.ts
+++ server/src/email/email.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Email } from './models/email.model';
+import { Recipients } from './models/recipient.model';
+import { Conversations } from './models/conversation.model';
+import { Attachments } from './models/attachment.model';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Email])],
+  imports: [SequelizeModule.forFeature([Email, Recipients, Conversations, Attachments])],
   controllers: [EmailController],
   providers: [EmailService],
 })
